8b2f33aced4b924a0f5893e8bd825e0e0a34e928
Kishan Vishwakarma <kishanv1464@gmail.com>
2024-05-20 13:54:40 +0530

    Add files via upload

---
+++ models/Task.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Task', taskSchema);
